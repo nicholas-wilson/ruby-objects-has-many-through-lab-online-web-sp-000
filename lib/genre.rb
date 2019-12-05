f806ab2fb1db8cd5555595b4ac47d273e7e7b897
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-05 08:38:09 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -6,4 +6,20 @@ class Genre
     @name = name
     @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.collect do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    songs.collect do |song|
+      song.artist
+    end
+  end
 end
