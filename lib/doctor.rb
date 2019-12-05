ae80b11f0938768e9ae4391d261d89db7bb86a73
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-05 08:49:42 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,4 +14,16 @@ class Doctor
   def new_appointment(patient, date)
     Appointment.new(date, patient, self)
   end
+
+  def appointments
+    Appoinment.all.collect do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def patients
+    appointments.map do |appointment|
+      appointment.patient
+    end
+  end
 end
